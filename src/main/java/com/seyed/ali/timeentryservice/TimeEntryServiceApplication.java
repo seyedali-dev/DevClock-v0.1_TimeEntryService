bdccce9a85deb09d43b8e3b9c52b42b8ec7bfe0b
SayedAli <sayedxali7@gmail.com>
2024-05-09 10:34:25 +0330

    time-entry-service initial commit

---
+++ src/main/java/com/seyed/ali/timeentryservice/TimeEntryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.seyed.ali.timeentryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeEntryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeEntryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seyed/ali/timeentryservice/TimeEntryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seyed.ali.timeentryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeEntryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
